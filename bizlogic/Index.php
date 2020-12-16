5fa9a5bcf93de6a029d9e5d9b86a57910cf1d344
DiliniP <diliniprasadi95@gmail.com>
2020-12-16 12:35:26 +0530

    initial commit

--- bizlogic/Index.php
+++ bizlogic/Index.php
@@ -1,1 +1,49 @@
 <?php
+
+$host = "localhost"; 
+$user = "root"; 
+$password = ""; 
+$dbname = "structure"; 
+$id = '';
+
+$con = mysqli_connect($host, $user, $password,$dbname);
+
+$method = $_SERVER['REQUEST_METHOD'];
+$request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
+
+
+if (!$con) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+
+switch ($method) {
+    case 'GET':
+      $id = $_GET['id'];
+      $sql = "select * from user".($id?" where id=$id":''); 
+      break;
+}
+
+$result = mysqli_query($con,$sql);
+
+if (!$result) {
+  http_response_code(404);
+  die(mysqli_error($con));
+}
+
+if ($method == 'GET') {
+    if (!$id) echo '[';
+    for ($i=0 ; $i<mysqli_num_rows($result) ; $i++) {
+      echo ($i>0?',':'').json_encode(mysqli_fetch_object($result));
+    }
+    if (!$id) echo ']';
+  } elseif ($method == 'POST') {
+    echo json_encode($result);
+  } else {
+    echo mysqli_affected_rows($con);
+  }
+
+$con->close();
+
+
+?>
